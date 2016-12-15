cdcaaf74ec77e4ca916191bc7fe93080d1ca73e8
Robert Koch <github@kochie.io>
2016-12-15 15:21:08 +1100

    built gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+const gulp = require('gulp');
+const sass = require('gulp-ruby-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
+const jshint = require('gulp-jshint');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+const rename = require('gulp-rename');
+const concat = require('gulp-concat');
+const notify = require('gulp-notify');
+const cache = require('gulp-cache');
+const livereload = require('gulp-livereload');
+const del = require('del');
+
+gulp.task('styles', function() {
+    return sass('src/styles/main.scss', { style: 'expanded' })
+        .pipe(autoprefixer('last 2 version'))
+        .pipe(gulp.dest('dist/assets/css'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(cssnano())
+        .pipe(gulp.dest('dist/assets/css'))
+        .pipe(notify({ message: 'Styles task complete' }));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src('src/scripts/**/*.js')
+        .pipe(jshint('.jshintrc'))
+        .pipe(jshint.reporter('default'))
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('dist/assets/js'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/assets/js'))
+        .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+gulp.task('images', function() {
+    return gulp.src('src/images/**/*')
+        .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+        .pipe(gulp.dest('dist/assets/img'))
+        .pipe(notify({ message: 'Images task complete' }));
+});
+
+gulp.task('clean', function() {
+    return del(['dist/assets/css', 'dist/assets/js', 'dist/assets/img']);
+});
+
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles', 'scripts', 'images');
+});
+
+gulp.task('watch', function() {
+    // Watch .scss files
+    gulp.watch('src/styles/**/*.scss', ['styles']);
+    // Watch .js files
+    gulp.watch('src/scripts/**/*.js', ['scripts']);
+    // Watch image files
+    gulp.watch('src/images/**/*', ['images']);
+    // Create LiveReload server
+    livereload.listen();
+    // Watch any files in dist/, reload on change
+    gulp.watch(['dist/**']).on('change', livereload.changed);
+});
+
